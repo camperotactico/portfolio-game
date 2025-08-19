7986e30ae9efece7fd0011c5ba2a0f67ae9dddf3
Alberto Gómez <camperotactico@murena.io>
2025-08-19 16:45:45 +0200

    Add setup and teardown methods.

--- Assets/Tests/PlayMode/GameTimerTestScript.cs
+++ Assets/Tests/PlayMode/GameTimerTestScript.cs
@@ -9,12 +9,33 @@ public class GameTimerTestScript
 
     private const float startDuration = 2.5f;
     private const float waitForUnityEventTime = 0.2f;
+    private GameTimer gameTimer;
+
+    private GameTimer GetGameTimer()
+    {
+        GameObject gameObject = new GameObject();
+        GameTimer gameTimer = gameObject.AddComponent<GameTimer>();
+        return gameTimer;
+    }
+
+
+    [SetUp]
+    public void SetUp()
+    {
+        gameTimer = GetGameTimer();
+    }
+
+    [TearDown]
+    public void OneTimeTearDown()
+    {
+        gameTimer.Began.RemoveAllListeners();
+        gameTimer = null;
+    }
+
 
     [UnityTest]
     public IEnumerator BeginTimerTest([Values(startDuration)] float startDuration, [Values(waitForUnityEventTime)]  float waitForUnityEventTime)
     {
-        GameObject gameObject = new GameObject();
-        GameTimer gameTimer = gameObject.AddComponent<GameTimer>();
 
         bool gameTimerBeganEventFired = false;
         float waitForUnityEventTimer = waitForUnityEventTime;
@@ -27,18 +48,33 @@ public class GameTimerTestScript
             waitForUnityEventTimer -= Time.deltaTime;
         }
 
-        gameTimer.Began.RemoveAllListeners();
         Assert.IsTrue(gameTimerBeganEventFired,"`GameTimer.Began()` event was not fired when `gameTimer.Begin(startDuration)` was called");
-        Assert.IsTrue(gameTimer.IsRunning(),"1GameTimer.IsRunning()` should return `true` when the timer began.");
+        Assert.IsTrue(gameTimer.IsRunning(),"GameTimer.IsRunning()` should return `true` when the timer began.");
         yield return null;
         
     }
 
     [UnityTest]
-    public IEnumerator StopTimerTest()
+    public IEnumerator StopTimerTest([Values(startDuration)] float startDuration, [Values(waitForUnityEventTime)] float waitForUnityEventTime)
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
+        bool gameTimerStoppedEventCalled = false;
+        float waitForUnityEventTimer = waitForUnityEventTime;
+        gameTimer.Stopped.AddListener(() => gameTimerStoppedEventCalled = true);
+
+        gameTimer.Begin(startDuration);
+        yield return new WaitForEndOfFrame();
+        gameTimer.Stop();
+
+        while (!gameTimerStoppedEventCalled && waitForUnityEventTimer >= 0)
+        {
+            yield return new WaitForEndOfFrame();
+            waitForUnityEventTimer -= Time.deltaTime;
+        }
+
+        Assert.IsTrue(gameTimerStoppedEventCalled, "`GameTimer.Stopped()` event was not called when `gameTimer.Stop()` was called");
+        Assert.IsTrue(gameTimer.IsRunning(), "GameTimer.IsRunning()` should return `false` after the timer has been stopped.");
+
+
         yield return null;
     }
 
@@ -47,6 +83,18 @@ public class GameTimerTestScript
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator StopStoppedTimerTest()
+    {
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator StartStartedTimerTest()
+    {
         yield return null;
     }
 }
