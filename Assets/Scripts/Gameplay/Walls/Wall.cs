bf5dcc20b3886f5825dc6d3220f74c0d8e49f784
Alberto Gómez <camperotactico@murena.io>
2025-08-28 11:52:28 +0200

    Updated WIP wall movement.

--- Assets/Scripts/Gameplay/Walls/Wall.cs
+++ Assets/Scripts/Gameplay/Walls/Wall.cs
@@ -8,105 +8,35 @@ public class Wall : MonoBehaviour
     [SerializeField]
     private Rigidbody2D bottomHalf;
 
-
-    private float currentVerticalPosition;
-    private float currentGoalSize;
-
-    private bool hasPositionChanged = false;
-    private Coroutine moveVerticalPositionCoroutine;
-    private Coroutine setGoalSizeCoroutine;
+    // TODO: Create a WallVerticalPositionProvider
+    // TODO: Create a WallGoalSizeProvider
+    private WallMovementController movementController;
 
 
     private void Awake()
     {
-        currentVerticalPosition = 0f;
-        currentGoalSize = 0f;
+        movementController = new WallMovementController(transform, topHalf, bottomHalf);
     }
 
     private void Start()
     {
-        UpdateWallPosition();
-        MoveTo(2f, 0.5f);
-        SetGoalSize(6f, 3f);
+        // TODO: Replace this with two different routines, one for the vertical position that uses the WallVerticalPositionProvider and another for the goal size that uses WallGoalSizeProvider
+        StartCoroutine(PingPong());
+    }
+
+    IEnumerator PingPong()
+    {
+        float targetVerticalPosition = 5f;
+        yield return movementController.SetGoalSize(8f, 2f);
+        while (true)
+        {
+            yield return movementController.MoveTo(targetVerticalPosition, 5f);
+            targetVerticalPosition *= -1;
+        }
     }
 
     private void FixedUpdate()
     {
-        if (!hasPositionChanged)
-        {
-            return;
-        }
-        UpdateWallPosition();
-        hasPositionChanged = false;
-    }
-
-    private void MoveTo(float newVerticalPosition, float duration = 0f)
-    {
-        if (moveVerticalPositionCoroutine != null)
-        {
-            StopCoroutine(moveVerticalPositionCoroutine);
-        }
-
-        moveVerticalPositionCoroutine = StartCoroutine(HandleVerticalPosition(newVerticalPosition,duration));
-    }
-
-    private IEnumerator HandleVerticalPosition(float newVerticalPosition, float duration = 0f)
-    {
-        float startVerticalPosition = currentVerticalPosition;
-        float timer = 0f;
-
-        while (timer < duration)
-        {
-            yield return new WaitForFixedUpdate();
-            timer = Mathf.Min(timer +Time.fixedDeltaTime, duration);
-            currentVerticalPosition = Mathf.Lerp(startVerticalPosition, newVerticalPosition, timer / duration);
-            hasPositionChanged = true;
-
-        }
-        currentVerticalPosition =  newVerticalPosition;
-        hasPositionChanged = true;
-
-        moveVerticalPositionCoroutine = null;
-        yield return null;
-
-    }
-
-    private void SetGoalSize(float newGoalSize, float duration = 0f)
-    {
-        if (setGoalSizeCoroutine != null)
-        {
-            StopCoroutine(setGoalSizeCoroutine);
-        }
-
-        setGoalSizeCoroutine = StartCoroutine(HandleGoalSize(newGoalSize, duration));
-    }
-
-    private IEnumerator HandleGoalSize(float newGoalSize, float duration = 0f)
-    {
-        float startGoalSize = currentGoalSize;
-        float timer = 0f;
-
-        while (timer < duration)
-        {
-            yield return new WaitForFixedUpdate();
-            timer = Mathf.Min(timer + Time.fixedDeltaTime, duration);
-            currentGoalSize = Mathf.Lerp(startGoalSize, newGoalSize, timer / duration);
-            hasPositionChanged = true;
-
-        }
-        currentGoalSize = newGoalSize;
-        hasPositionChanged = true;
-
-        setGoalSizeCoroutine = null;
-        yield return null;
-
-    }
-
-    private void UpdateWallPosition()
-    {
-        Vector2 tophalfPosition = transform.TransformPoint(0f, currentVerticalPosition + (0.5f* currentGoalSize), 0f);
-        Vector2 bottomHalfPosition = transform.TransformPoint(0f, currentVerticalPosition - (0.5f* currentGoalSize), 0f);
-        topHalf.MovePosition(tophalfPosition);
-        bottomHalf.MovePosition(bottomHalfPosition);
+        movementController.UpdateWallPosition();
     }
 }
---
+++ Assets/Scripts/Gameplay/Walls/WallMovementController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using UnityEngine;
+
+public class WallMovementController
+{
+    private Transform transform;
+    private Rigidbody2D topHalf;
+    private Rigidbody2D bottomHalf;
+
+    private float currentVerticalPosition;
+    private float currentGoalSize;
+    private bool hasPositionChanged = false;
+
+    public WallMovementController(Transform newTransform, Rigidbody2D newTopHalf, Rigidbody2D newBottomHalf)
+    {
+        transform = newTransform;
+        topHalf = newTopHalf;
+        bottomHalf = newBottomHalf;
+        currentVerticalPosition = 0f;
+        currentGoalSize = 0f;
+        hasPositionChanged = true;
+    }
+
+    public IEnumerator MoveTo(float newVerticalPosition, float duration = 0f)
+    {
+        float startVerticalPosition = currentVerticalPosition;
+        float timer = 0f;
+
+        while (timer < duration)
+        {
+            yield return new WaitForFixedUpdate();
+            timer = Mathf.Min(timer +Time.fixedDeltaTime, duration);
+            currentVerticalPosition = Mathf.Lerp(startVerticalPosition, newVerticalPosition, timer / duration);
+            hasPositionChanged = true;
+
+        }
+        currentVerticalPosition =  newVerticalPosition;
+        hasPositionChanged = true;
+
+        yield return null;
+
+    }
+
+    public IEnumerator SetGoalSize(float newGoalSize, float duration = 0f)
+    {
+        float startGoalSize = currentGoalSize;
+        float timer = 0f;
+
+        while (timer < duration)
+        {
+            yield return new WaitForFixedUpdate();
+            timer = Mathf.Min(timer + Time.fixedDeltaTime, duration);
+            currentGoalSize = Mathf.Lerp(startGoalSize, newGoalSize, timer / duration);
+            hasPositionChanged = true;
+
+        }
+        currentGoalSize = newGoalSize;
+        hasPositionChanged = true;
+        yield return null;
+
+    }
+
+    public void UpdateWallPosition()
+    {
+        if (!hasPositionChanged)
+        {
+            return;
+        }
+
+        Vector2 tophalfPosition = transform.TransformPoint(0f, currentVerticalPosition + (0.5f* currentGoalSize), 0f);
+        Vector2 bottomHalfPosition = transform.TransformPoint(0f, currentVerticalPosition - (0.5f* currentGoalSize), 0f);
+        topHalf.MovePosition(tophalfPosition);
+        bottomHalf.MovePosition(bottomHalfPosition);
+        hasPositionChanged = false;
+    }
+}
